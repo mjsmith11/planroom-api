0ce7b88eebf7edbcff3931dc72331a55c99182e0
Matt Smith <mjsmith92@outlook.com>
2018-09-05 21:09:36 -0400

    Move require into route group

--- src/routes.php
+++ src/routes.php
@@ -3,11 +3,11 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-require_once(__DIR__ . "/db/orchestrators/jobOrch.php");
-
 // Routes
 
 $app->group('/jobs', function(){
+    require_once(__DIR__ . "/db/orchestrators/jobOrch.php");
+    
     $this->post('', function($request, $response, $args){
         $in = $request->getParsedBody();
         $out = JobOrch::Create($in);
