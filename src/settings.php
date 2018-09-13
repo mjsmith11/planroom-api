e459525ae3b9d0e97392b2516bd44e222bafc00a
Matt Smith <mjsmith92@outlook.com>
2018-09-12 22:43:16 -0400

    Fix settings.php linting

--- src/settings.php
+++ src/settings.php
@@ -1,15 +1,15 @@
 <?php
 require_once(__DIR__ . "/config/configReader.php");
 return [
-    'settings' => [
-        'displayErrorDetails' => ConfigReader::getDisplayErrorDetails(),
-        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+	'settings' => [
+		'displayErrorDetails' => ConfigReader::getDisplayErrorDetails(),
+		'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
-        // Monolog settings
-        'logger' => [
-            'name' => 'slim-app',
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
-        ],
-    ],
+		// Monolog settings
+		'logger' => [
+			'name' => 'slim-app',
+			'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+			'level' => \Monolog\Logger::DEBUG,
+		],
+	],
 ];
