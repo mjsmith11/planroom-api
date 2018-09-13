822c6d54b760d0052b077433604078ac94131a59
Matt Smith <mjsmith92@outlook.com>
2018-09-12 22:42:31 -0400

    Fix middleware.php lint

--- src/middleware.php
+++ src/middleware.php
@@ -1,20 +1,26 @@
 <?php
+/**
+ * @SuppressWarnings checkUnusedVariables
+ * Middleware functions
+ */
+
 require_once(__DIR__ . "/config/configReader.php");
 // Application middleware
 
 // CORS Header Middleware
 $app->add(function($req, $res, $next) {
-    $response = $next($req,$res);
-    
-    //Look for Origin in Origins that are allowed
-    $http_origin = $_SERVER['HTTP_ORIGIN'];
-    $cors_origins = ConfigReader::getCorsOrigins();
-    if (in_array($http_origin, $cors_origins, true)) {
-        $response = $response->withHeader("Access-Control-Allow-Origin", $http_origin);
-    }
+	$response = $next($req, $res);
+	
+	//Look for Origin in Origins that are allowed
+	$httpOrigin = $_SERVER['HTTP_ORIGIN'];
+	$corsOrigins = ConfigReader::getCorsOrigins();
+	$shouldAddHeader = in_array($httpOrigin, $corsOrigins, true);
+	if ($shouldAddHeader) {
+		$response = $response->withHeader("Access-Control-Allow-Origin", $httpOrigin);
+	}
 
-    //Add headers that are the same every time
-    return $response->withHeader("Access-Control-Allow-Headers", "Content-Type, Accept")
-                    ->withHeader("Access-Control-Max-Age", "86400")
-                    ->withHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	//Add headers that are the same every time
+	return $response->withHeader("Access-Control-Allow-Headers", "Content-Type, Accept")
+					->withHeader("Access-Control-Max-Age", "86400")
+					->withHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 });
