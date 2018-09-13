04793edf46ea434be069793b7a7ec394c4864f13
Matt Smith <mjsmith92@outlook.com>
2018-09-12 22:23:00 -0400

    Job Orch linting

--- src/db/orchestrators/jobOrch.php
+++ src/db/orchestrators/jobOrch.php
@@ -1,6 +1,10 @@
 <?php
-    require_once(__DIR__ . '/../base/orch.php');
-    class JobOrch extends BaseOrch {
-        protected static $tableName = "job";
-        protected static $fieldList = array("name", "bidDate", "subcontractorBidsDue", "prebidDateTime","prebidAddress","bidEmail","bonding","taxible");
-    }
+	require_once(__DIR__ . '/../base/orch.php');
+	/**
+	 * @SuppressWarnings checkUnusedVariables
+	 * Orchestrator for Jobs
+	 */
+	class JobOrch extends BaseOrch {
+		protected static $tableName = "job";
+		protected static $fieldList = array("name", "bidDate", "subcontractorBidsDue", "prebidDateTime", "prebidAddress", "bidEmail", "bonding", "taxible");
+	}
