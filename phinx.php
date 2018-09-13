a65015d27a5fda9a2f8fee6d770ad8ff048ad062
Matt Smith <mjsmith92@outlook.com>
2018-09-13 09:33:07 -0400

    fix phinx.php linting

--- phinx.php
+++ phinx.php
@@ -1,15 +1,15 @@
 <?php
 $dbSettings = require __DIR__ . '/db/connection.php';
 return  [
-            'environments' => [
-                'default_database' => 'app-db',
-                'app-db' => [
-                    'name' => $dbSettings['dbName'],
-                    'connection' => $dbSettings['conn']
-                ]
-            ],
-            'paths' => [
-                'migrations' => __DIR__ . '/db/migrations'
-            ]
+			'environments' => [
+				'default_database' => 'app-db',
+				'app-db' => [
+					'name' => $dbSettings['dbName'],
+					'connection' => $dbSettings['conn']
+				]
+			],
+			'paths' => [
+				'migrations' => __DIR__ . '/db/migrations'
+			]
 
-        ];
+		];
