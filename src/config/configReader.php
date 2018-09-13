71ec9b3ded657ae0252f9d675aa38ac32c55dd1e
Matt Smith <mjsmith92@outlook.com>
2018-09-13 09:42:49 -0400

    lint errors configReader

--- src/config/configReader.php
+++ src/config/configReader.php
@@ -1,33 +1,44 @@
 <?php
-    // Singleton location for nonsensitive configuration data
-    class ConfigReader {
-        private static $readDone = false;
-        private static $corsOrigins;
-        private static $displayErrorDetails;
+	/**
+	 *  Singleton location for nonsensitive configuration data
+	 */
+	class ConfigReader {
+		private static $readDone = false;
+		private static $corsOrigins;
+		private static $displayErrorDetails;
 
-        public static function getCorsOrigins() {
-            if (!self::$readDone) {
-                self::read();
-            }
-            return self::$corsOrigins;
-        }
+		/**
+		 * Gets the allowable origins for CORS requests
+		 * @returns array of allowed CORS origins 
+		 */
+		public static function getCorsOrigins() {
+			if (!self::$readDone) {
+				self::_read();
+			}
+			return self::$corsOrigins;
+		}
 
-        public static function getDisplayErrorDetails() {
-            if (!self::$readDone) {
-                self::read();
-            }
-            return self::$displayErrorDetails;
-        }
-        
-        private static function read() {
-            $json_string = file_get_contents(__DIR__ . '/../../config.json');
-            $config = json_decode($json_string, true);
+		/**
+		 * Gets configuration for displaying of error details
+		 * 
+		 * @returns boolean should error details be displayed
+		 */
+		public static function getDisplayErrorDetails() {
+			if (!self::$readDone) {
+				self::_read();
+			}
+			return self::$displayErrorDetails;
+		}
+		
+		private static function _read() {
+			$jsonString = file_get_contents(__DIR__ . '/../../config.json');
+			$config = json_decode($jsonString, true);
 
-            self::$corsOrigins = $config['cors_origins'];
-            self::$displayErrorDetails = $config['display_error_details'];
+			self::$corsOrigins = $config['cors_origins'];
+			self::$displayErrorDetails = $config['display_error_details'];
 
-            unset($config);
-            self::$readDone = true;
-        }
+			unset($config);
+			self::$readDone = true;
+		}
 
-    }
+	}
