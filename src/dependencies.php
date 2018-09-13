63bfd9949eb5cf47ea8667f7421aeaba9ec641c3
Matt Smith <mjsmith92@outlook.com>
2018-09-12 22:45:38 -0400

    Fix dependencies.php linting

--- src/dependencies.php
+++ src/dependencies.php
@@ -1,13 +1,16 @@
 <?php
-// DIC configuration
+/**
+ * @SuppressWarnings checkUnusedVariables
+ * DIC configuration
+ */
 
 $container = $app->getContainer();
 
 // monolog
-$container['logger'] = function ($c) {
-    $settings = $c->get('settings')['logger'];
-    $logger = new Monolog\Logger($settings['name']);
-    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
-    return $logger;
+$container['logger'] = function ($cont) {
+	$settings = $cont->get('settings')['logger'];
+	$logger = new Monolog\Logger($settings['name']);
+	$logger->pushProcessor(new Monolog\Processor\UidProcessor());
+	$logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
+	return $logger;
 };
