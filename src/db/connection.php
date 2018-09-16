23643237f18948536b17091c189ca2c47e13dc7e
Matt Smith <mjsmith92@outlook.com>
2018-09-16 17:22:12 -0400

    Fix dsn in connection

--- src/db/connection.php
+++ src/db/connection.php
@@ -32,7 +32,7 @@
 				$pass    = $config['mysql']['password'];
 				$charset = 'utf8';
 
-				$dsn = 'mysql:host=$host;dbname=$db;charset=$charset';
+				$dsn = 'mysql:host=' . $host . ';dbname=' . $db . ';charset=' . $charset;
 				$opt = [
 					PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
 					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
