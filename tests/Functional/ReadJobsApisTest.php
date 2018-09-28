e7ecf5c95dd812f8bbdc855b4dd8b21c38d9db28
Matt Smith <mjsmith92@outlook.com>
2018-09-27 22:16:15 -0400

    Single Job API Test

---
+++ tests/Functional/ReadJobsApisTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Tests\Functional;
+
+require_once(__DIR__ . "/../../src/db/connection.php");
+use Connection;
+
+/**
+ * Test Routes that read jobs
+ * @SuppressWarnings checkProhibitedFunctions
+ */
+class ReadJobsApisTest extends BaseTestCase {
+	private $pdo;
+	private static $fileBackup;
+	private static $filePath = __DIR__ . '/../../config.json';
+	
+
+	/**
+	 * Set up for tests. Backup config file and delete it if it exists
+	 */
+	public static function setUpBeforeClass() {
+		if (file_exists(self::$filePath)) {
+			self::$fileBackup = file_get_contents(self::$filePath);
+			unlink(self::$filePath);
+		}
+		$config = array();
+		$config['display_error_details'] = true;
+		$config['cors_origins'] = array();
+		$config['mysql'] = array();
+		$config['logging'] = array('level' => 'debug', 'maxFiles' => 1);
+
+		$file = fopen(self::$filePath, 'w');
+		fwrite($file, json_encode($config));
+		fclose($file);
+	}
+
+	/**
+	 * After tests: Restore config file if it was backed up
+	 */
+	public static function tearDownAfterClass() {
+		unlink(self::$filePath);
+		if (isset(self::$fileBackup)) {
+			$file = fopen(__DIR__ . '/../../config.json', 'w');
+			fwrite($file, self::$fileBackup);
+			fclose($file);
+		}
+	}
+
+	/**
+	 * Set up test connection
+	 */
+	public function setUp() {
+		$this->pdo = Connection::getConnection(true)['conn'];
+	}
+
+	/**
+	 * Test route /jobs/{id}
+	 */
+	public function testReadSingleJob() {
+		$readMockResult = [[ 
+			'id' => 45, 
+			'name' => 'jobName',
+			'bidDate' => '08-19-2019',
+			'subcontractorBidsDue' => '05-16-2018T13:00',
+			'prebidDateTime' => '06-02-1999T08:00',
+			'prebidAddress' => '123 Main St.',
+			'bidEmail' => 'abc@xyz.com',
+			'bonding' => 1,
+			'taxible' => 0 
+		]];
+
+		$this->pdo->mock("SELECT * FROM job WHERE `id` = :id", $readMockResult);
+		
+		$response = $this->runApp('GET', '/jobs/45');
+		
+		$this->assertEquals(200, $response->getStatusCode());
+
+		$parsedResp = json_decode((string)$response->getBody(), true);
+		
+		$this->assertEquals(45, $parsedResp['id'], 'returned id');
+		$this->assertEquals('08-19-2019', $parsedResp['bidDate'], 'returned bidDate');
+		$this->assertEquals('05-16-2018T13:00', $parsedResp['subcontractorBidsDue'], 'returned subcontractorBidsDue');
+		$this->assertEquals('06-02-1999T08:00', $parsedResp['prebidDateTime'], 'returned prebidDateTime');
+		$this->assertEquals('123 Main St.', $parsedResp['prebidAddress'], 'returned prebidAddress');
+		$this->assertEquals('abc@xyz.com', $parsedResp['bidEmail'], 'returned bidEmail');
+		$this->assertEquals(1, $parsedResp['bonding'], 'returned bonding');
+		$this->assertEquals(0, $parsedResp['taxible'], 'returned taxible');
+	}
+
+}
