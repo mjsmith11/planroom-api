e50b31c48cf8a11c2b1592cb3a6ff125732e84ed
Matt Smith <mjsmith92@outlook.com>
2018-10-03 22:49:01 -0400

    Add Credential Provider For AWS

--- src/s3/credentialProvider.php
+++ src/s3/credentialProvider.php
@@ -1,32 +1,36 @@
 <?php
-    namespace s3;
+    namespace Planroom\S3;
 
     use GuzzleHttp\Promise;
     use GuzzleHttp\Promise\RejectedPromise;
+    use Aws\Credentials\Credentials;
+    use Aws\Credentials\CredentialsException;
 
     class CredentialProvider {
-        public static function Provider($container)
+        public static function json($container)
         {
-            $container['logger']->debug("Reading AWS Credentials");
-            try {
-                $jsonString = file_get_contents(__DIR__ . '/../../config.json');
-                $config = json_decode($jsonString, true);
-                $key = $config['aws']['key'];
-                $secret = $config['aws']['secret'];
-                unset($config);
-                if ($key && $secret) {
-                    return Promise\promise_for(
-                        new Credentials($key, $secret)
-                    );
-                } else {
+            return function () use ($container) {
+                $container['logger']->debug("Reading AWS Credentials");
+                try {
+                    $jsonString = file_get_contents(__DIR__ . '/../../config.json');
+                    $config = json_decode($jsonString, true);
+                    $key = $config['aws']['key'];
+                    $secret = $config['aws']['secret'];
+                    unset($config);
+                    if ($key && $secret) {
+                        return Promise\promise_for(
+                            new Credentials($key, $secret)
+                        );
+                    } else {
+                        unset($key, $secret);
+                        $container['logger']->alert('AWS Credentials Not Found');
+                        return new RejectedPromise(new CredentialsException('Could not find credentials in config.json'));
+                    }
+                } catch (\Throwable $e) {
                     unset($key, $secret);
-                    $container['logger']->alert('AWS Credentials Not Found');
-                    return new RejectedPromise(new CredentialsException('Could not find credentials in config.json'));
+                    $container['logger']->alert('Exception reading AWS Credentials', array('message' => $e->getMessage()));
+                    return new RejectedPromise(new CredentialsException('Error parsing config file'));
                 }
-            } catch (\Throwable $e) {
-                unset($key, $secret);
-                $container['logger']->alert('Exception reading AWS Credentials', array('message' => $e.getMessage()));
-                return new RejectedPromise(new CredentialsException('Error parsing config file'));
-            }
+            };
         }
     }
