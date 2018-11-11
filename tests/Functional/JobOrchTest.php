7f4670335aa38c19a4859323b0149328bc65c972
Matt Smith <mjsmith92@outlook.com>
2018-11-11 15:30:29 -0500

    New test in JobOrchTest

--- tests/Functional/JobOrchTest.php
+++ tests/Functional/JobOrchTest.php
@@ -8,6 +8,8 @@ require_once(__DIR__ . "/testDependenciesContainer.php");
 use JobOrch;
 use Connection;
 use TestContainer;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 /**
  * Tests for the Base Orch
@@ -35,5 +37,36 @@ class JobOrchTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals('fakeData2', $result['field2'], 'Read fakeData2');
 		$this->assertEquals('fakeData3', $result['field3'], 'Read fakeData3');
 
+	}
+
+	public function testSendInvitations() {
+		$readMockResult = [[ 
+			'id' => 45, 
+			'name' => 'jobName',
+			'bidDate' => '08-19-2019',
+			'subcontractorBidsDue' => '05-16-2018T13:00',
+			'prebidDateTime' => '06-02-1999T08:00',
+			'prebidAddress' => '123 Main St.',
+			'bidEmail' => 'abc@xyz.com',
+			'bonding' => 1,
+			'taxible' => 0 
+		]];
+		$this->pdo->mock("SELECT * FROM job WHERE `id` = :id", $readMockResult);
+
+		$container = TestContainer::getContainer();
+		$stub = $this->createMock(PHPMailer::class);
+		$stub->method('clearAddresses')
+			->willReturn('');
+		$stub->method('addAddress')
+			->willReturn('');
+		$stub->method('isHTML')
+			->willReturn('');
+		$stub->method('send')
+			->willReturn('');
+		unset($container['mailer']);
+		$container['mailer'] = $stub;
+		$emails = array('test1@test.com', 'test2@test.com');
+		JobOrch::sendInvitations(45, 3, $emails, $container);
+		// It's tough to assert something here, but this shows it won't error
 	}
 }
