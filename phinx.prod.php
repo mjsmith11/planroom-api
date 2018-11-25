803fc9e251733461808aee2db2a9660e146db6f1
Matt Smith <mjsmith92@outlook.com>
2018-11-24 21:42:13 -0500

    Fix phinx for production

---
+++ phinx.prod.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__ . '/../../src/db/connection.php';
+$dbSettings = Connection::getConnection(null);
+return  [
+			'environments' => [
+				'default_database' => 'app-db',
+				'app-db' => [
+					'name' => $dbSettings['dbName'],
+					'connection' => $dbSettings['conn']
+				]
+			],
+			'paths' => [
+				'migrations' => __DIR__ . '/../../src/db/migrations'
+			]
+
+		];
