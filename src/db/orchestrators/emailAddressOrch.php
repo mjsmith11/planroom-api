c40a4a97174ca6367a6722a15d0579659536e511
Matt Smith <mjsmith92@outlook.com>
2019-01-29 22:31:55 -0500

    Write Address Exists Method

--- src/db/orchestrators/emailAddressOrch.php
+++ src/db/orchestrators/emailAddressOrch.php
@@ -9,4 +9,23 @@
 		protected static $tableName = "email_address";
         protected static $fieldList = array("address", "uses");
         
+
+        /**
+		 * Checks if a address exists in the database
+		 * 
+		 * @param address the address to check
+		 * @param container dependency container
+		 * 
+		 * @return boolean
+		 */
+        public static function addressExists($address, $container) {
+            $container['logger']->info("Checking email address existence", array('address' => $address));
+            $pdo = Connection::getConnection($container)['conn'];
+			$sql = "SELECT * FROM " . static::$tableName . " WHERE `address` = :address";
+            $container['logger']->debug("Address exists query built", array('sql' => $sql));
+            $statement = $pdo->prepare($sql);
+			$statement->bindParam("address", $address);
+			$statement->execute();
+			return $statement->fetchColumn() > 0;
+        }
     }
