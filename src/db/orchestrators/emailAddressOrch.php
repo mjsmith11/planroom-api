97e98f3cdb8a077c5eae8a02f2f44548338bae7c
Matt Smith <mjsmith92@outlook.com>
2019-02-12 21:33:49 -0500

    Add readByAddress and recordUse to emailAddressOrch

--- src/db/orchestrators/emailAddressOrch.php
+++ src/db/orchestrators/emailAddressOrch.php
@@ -10,6 +10,33 @@
         protected static $fieldList = array("address", "uses");
         
 
+		/**
+		 * Add a record for new addresses and 
+		 * 
+		 * @param address the address to check
+		 * @param container dependency container
+		 * 
+		 * @return email_address object
+		 */
+		public static function recordUse($address, $container) {
+			$container['logger']->info("Recording email address use", array('address' => $address));
+			if (self::addressExists($address, $container)) {
+				$obj = self::readByAddress($address, $container);
+				$obj['uses'] = $obj['uses'] + 1;
+				$pdo = Connection::getConnection($container)['conn'];
+				$sql = "UPDATE email_address SET uses = :uses WHERE `address` = :address";
+				$container['logger']->debug('Update query for record use built', array('sql' => $sql));
+				$statement = $pdo->prepare($sql);
+				$statement->bindParam("uses", $obj['uses']);
+				$statement->bindParam("address", $address);
+				$statement->execute();
+				return self::read($obj['id'], $container);
+			} else {
+				$obj = array('address' => $address, 'uses' => 1);
+				return self::create($obj, $container);
+			}
+		}
+
         /**
 		 * Checks if a address exists in the database
 		 * 
@@ -21,11 +48,30 @@
         public static function addressExists($address, $container) {
             $container['logger']->info("Checking email address existence", array('address' => $address));
             $pdo = Connection::getConnection($container)['conn'];
-			$sql = "SELECT * FROM " . static::$tableName . " WHERE `address` = :address";
+			$sql = "SELECT * FROM email_address WHERE `address` = :address";
             $container['logger']->debug("Address exists query built", array('sql' => $sql));
             $statement = $pdo->prepare($sql);
 			$statement->bindParam("address", $address);
 			$statement->execute();
 			return $statement->fetchColumn() > 0;
+		}
+		
+		/**
+		 * Read a record by address
+		 * 
+		 * @param address the address to check
+		 * @param container dependency container
+		 * 
+		 * @return email_address object
+		 */
+        public static function readByAddress($address, $container) {
+            $container['logger']->info("Checking email address existence", array('address' => $address));
+            $pdo = Connection::getConnection($container)['conn'];
+			$sql = "SELECT * FROM email_address WHERE `address` = :address";
+            $container['logger']->debug("Read by address query built", array('sql' => $sql));
+            $statement = $pdo->prepare($sql);
+			$statement->bindParam("address", $address);
+			$statement->execute();
+			return $statement->fetch(PDO::FETCH_ASSOC);
         }
     }
